3ad70f74f36e39586ab76d10ff06287dfb843fcf
Austin <rockmanvnx6@gmail.com>
2020-03-18 23:38:32 +1100

    Update sensor.py

--- RBPI/sensor.py
+++ RBPI/sensor.py
@@ -9,7 +9,7 @@ import uuid
 ID=str(uuid.uuid4()).replace('-','')
 conn = sqlite3.connect('rbpi-rmit-iot.db')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS info (id TEXT PRIMARY KEY)")
+c.execute("CREATE TABLE IF NOT EXISTS info (id TEXT)")
 
 try:
 	c.execute("INSERT INTO info (id) VALUES ('" + ID + "')")
