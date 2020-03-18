9045c62680562bb064834d3c0d82921079d7ef4f
Austin <rockmanvnx6@gmail.com>
2020-03-18 22:08:20 +1100

    Update sensor.py

--- RBPI/sensor.py
+++ RBPI/sensor.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat;
 from datetime import datetime;
+import os
 import csv
 
 # Setup
@@ -10,7 +11,15 @@ temp = sense.get_temperature()
 dir_name = './data/'
 filename = dir_name + datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 
+# Initialize
+try:
+	os.makedirs(dir_name)
+except FileExistsError:
+	pass
+
 with open(filename, 'a+') as f:
 	writer = csv.writer(filename, delimiter=',', quotechar='"', 
 						quoting=csv.QUOTE_MINIMAL)
 	writer.writerow(['test', temp])
+
+
