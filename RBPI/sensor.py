db9009fbbad441bf84d8c396cb75c8d814621da3
Austin <rockmanvnx6@gmail.com>
2020-03-18 23:06:59 +1100

    Update sensor.py

--- RBPI/sensor.py
+++ RBPI/sensor.py
@@ -2,6 +2,21 @@ from sense_hat import SenseHat;
 from datetime import datetime;
 import os
 import csv
+import sqlite3
+import uuid
+
+# Serialisation
+ID=str(uuid.uuid4()).replace('-','')
+conn = sqlite3.connect('rbpi-rmit-iot.db')
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS info (id TEXT PRIMARY KEY)")
+
+try:
+	c.execute("INSERT INTO info VALUES ({id})".format(id=ID))
+except sqlite3.IntegrityError:
+	# ID already exists
+	c.execute("SELECT id FROM info")
+	print(c.fetchone())
 
 # Setup
 sense = SenseHat()
@@ -19,6 +34,8 @@ except FileExistsError:
 
 with open(filename, 'a+') as f:
 	writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(['test', temp])
+	if(f.tell() == 0):
+		f.write(['temp'])
+	writer.writerow([temp])
 
 
