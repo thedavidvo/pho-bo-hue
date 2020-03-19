c1c654c8a190d334ac34210725d9255592a95d5d
Austin <rockmanvnx6@gmail.com>
2020-03-19 17:27:17 +1100

    Update sensor.py

--- RBPI/sensor.py
+++ RBPI/sensor.py
@@ -12,7 +12,7 @@ c = conn.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS info (id TEXT PRIMARY KEY)")
 
 c.execute("SELECT id FROM info")
-print('ID' + c.fetchone())
+print(c.fetchone())
 
 try:
 	c.execute("INSERT INTO info (id) VALUES (?)", [ID])
